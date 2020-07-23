af8da97cee0f3128a6eba3b163a18472c891e197
josherias <josherias10@gmail.com>
2020-07-23 11:12:49 +0300

    deploy react calcultor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ClearButton } from "./components/ClearButton";
+import * as math from "mathjs";
+
+class App extends React.Component {
+  state = {
+    input: "",
+  };
+
+  handleInput = (val) => {
+    this.setState({ input: this.state.input + val });
+  };
+
+  handleEqual = () => {
+    this.setState({ input: math.evaluate(this.state.input) });
+  };
+
+  handleClear = () => {
+    this.setState({ input: "" });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="calc-wrapper">
+          <Input input={this.state.input}></Input>
+          <div className="row">
+            <Button updateInput={this.handleInput}>7</Button>
+            <Button updateInput={this.handleInput}>8</Button>
+            <Button updateInput={this.handleInput}>9</Button>
+            <Button updateInput={this.handleInput}>/</Button>
+          </div>
+
+          <div className="row">
+            <Button updateInput={this.handleInput}>4</Button>
+            <Button updateInput={this.handleInput}>5</Button>
+            <Button updateInput={this.handleInput}>6</Button>
+            <Button updateInput={this.handleInput}>X</Button>
+          </div>
+
+          <div className="row">
+            <Button updateInput={this.handleInput}>1</Button>
+            <Button updateInput={this.handleInput}>2</Button>
+            <Button updateInput={this.handleInput}>3</Button>
+            <Button updateInput={this.handleInput}>+</Button>
+          </div>
+
+          <div className="row">
+            <Button updateInput={this.handleInput}>.</Button>
+            <Button updateInput={this.handleInput}>0</Button>
+            <Button updateInput={this.handleEqual}>=</Button>
+            <Button updateInput={this.handleInput}>-</Button>
+          </div>
+          <ClearButton clearItems={this.handleClear}>Clear</ClearButton>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
