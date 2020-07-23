63527c8146bf6aa0399d373b7b5c14daf6dc0dd9
josherias <josherias10@gmail.com>
2020-07-23 11:29:21 +0300

    Updates

---
+++ precache-manifest.c9b4f5d121158bd12ffd6e970c94a92d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3362c7e16db990dd19e6045c848a867",
+    "url": "/new-react-calcultor/index.html"
+  },
+  {
+    "revision": "4a457286c6eecbd99707",
+    "url": "/new-react-calcultor/static/css/main.992a9b23.chunk.css"
+  },
+  {
+    "revision": "56521fa9045d923e7098",
+    "url": "/new-react-calcultor/static/js/2.18a7924b.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/new-react-calcultor/static/js/2.18a7924b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a457286c6eecbd99707",
+    "url": "/new-react-calcultor/static/js/main.20e386f2.chunk.js"
+  },
+  {
+    "revision": "b6ea333be630cae06e4e",
+    "url": "/new-react-calcultor/static/js/runtime-main.11ffeec5.js"
+  }
+]);
